ec9082629cacf11232aad5706c5826189b7cdbe0
mladenz23 <mladen.zeljkovic@gmail.com>
2024-11-12 11:30:31 +0100

    Add player choice and display symbol on the board

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # MZ-TicTacToe
+
+Tic Tac Toe game with vanilla HTML, CSS and JS
+
+It's a basic tic tac toe game, where user can pick
+cross or circle and then play against computer.
+Game keeps score of the winnings.
+
+- 11 / 12 / 2024
+  I've started this project a while back, but never
+  finished it. Starting now I currently have HTML
+  score interface, with buttons to chose cross or
+  circle and the playfield along with symbols (X, O).
+  I've added style with CSS and some functionality
+  with JS (player selection and showing symbol on
+  the board).
+
+  Things to do:
+
+  - Alternate between player and computer picks
+  - End when someone wins
+  - End when grid is full (win or tie)
+  - Update the scoreboard
+  - Start new round
+  - First to 5 rounds won wins the game
+  - Start new game (reset scoreboard)
---
+++ index.js
@@ -0,0 +1,56 @@
+'use sctrict';
+
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const cross = document.getElementById('cross');
+const circle = document.getElementById('circle');
+const fields = document.querySelectorAll('.field');
+const cont = document.getElementById('cont');
+const btnCross = document.querySelector('.btn--cross');
+const btnCircle = document.querySelector('.btn--circle');
+const display = document.getElementById('displayScore');
+const interface = document.getElementById('interface');
+
+let playerChoice = '';
+let npcChoice = '';
+let currentPlayer = '';
+
+const startGame = function () {
+  interface.addEventListener('click', function (e) {
+    if (e.target.classList.contains('btn-choose')) {
+      cont.classList.remove('hidden');
+
+      playerChoice = e.target.dataset.symbol;
+
+      enablePlayer();
+      enableNPC();
+    }
+  });
+};
+
+const enablePlayer = function () {
+  cont.addEventListener('click', function (e) {
+    if (e.target.classList.contains('field')) {
+      e.target.textContent = playerChoice === 'cross' ? '❌' : '⭕';
+    }
+  });
+};
+const randomField = Math.trunc(Math.random() * fields.length);
+
+const enableNPC = function () {
+  fields.forEach(field => {
+    if (field === '' && playerChoice === 'cross') {
+    }
+  });
+};
+
+const switchPlayer = function () {};
+
+startGame();
